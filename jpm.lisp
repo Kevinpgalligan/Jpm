5a995910fef24ce50d0f89c911e15031a2eb4928
Kevin Galligan <galligankevinp@gmail.com>
2025-07-19 13:35:05 +0100

    Parsing progress.

--- jpm.lisp
+++ jpm.lisp
@@ -4,60 +4,139 @@
 ;;  3. Manually implement closures in J.
 ;;  4. Automatic closure generation.
 
+;; Grammar TODO
+;;    - Various ways of defining functions
+;;     {{ }}
+;;     3 : 0
+;;     )
+;;   - control words (see "T-block" in notes)
+;;     if. else. elseif. assert. 
+;;     break. continue. for.
+;;     for_ijk. goto_lbl.
+;;     label_lbl.
+;;     return. select.
+;;     case.
+;;     fcase.
+;;     throw. catcht. try.
+;;     catch.
+;;     catchd.
+;;     catcht. while.
+;;     whilst.
+;;   - Other weird 'm : n' constructs
+
 (defpackage :jpm
   (:use :cl :esrap :alexandria))
 
 (in-package jpm)
 
-(defclass jobj ()
-  (()
-   (content :initarg :jthing :accessor content)))
 
+;;;; PARSE TREE DATA STRUCTURES.
+;;;; For when a list isn't enough.
+(defclass parse-node ()
+  ((children :initarg :children :reader children)))
+
+(defclass assignment-node (parse-node)
+  ((id :initarg :id :accessor id)
+   (op :initarg :op :accessor op)
+   (val :initarg :val :accessor val)))
+
+(defun make-assignment-node (list)
+  (destructuring-bind (id ws op val) list
+    (declare (ignorable ws))
+    (make-instance 'assignment-node :children list :id id :op op :val val)))
+
+
+;;;; TRAVERSAL INTERFACE.
+(defgeneric visit (visitor tree))
+
+;; By default, do nothin'.
+(defmethod visit (visitor tree)
+  (declare (ignore visitor tree)))
+
+;; But we do want to descend into the tree.
+(defmethod visit (visitor (tree list))
+  (dolist (child tree)
+    (visit visitor child)))
+(defmethod visit (visitor (node parse-node))
+  (visit visitor (children node)))
+
+
+;;;; OUTPUT.
+(defclass output-visitor ()
+  ((ostream :initarg :ostream :reader ostream)))
+
+(defmethod visit ((visitor output-visitor) (s string))
+  (format (ostream visitor) "~a" s))
+
+(defun serialise-j (ostream lines)
+  (let ((visitor (make-instance 'output-visitor :ostream ostream)))
+    (dolist (line lines)
+      (visit visitor line)
+      (format ostream "~%"))))
+
+
+;;;; GRAMMAR RULES.
 (defrule jscript
-    (? (and sentence (* (and nl sentence))))
+    (? sentences))
+
+(defrule sentences
+    (and sentence (* (and nl sentence)))
   (:lambda (list)
     (cons (first list)
-          (second list))))
+          ;; Remove the newlines.
+          (loop for line in (second list)
+                collect (second line)))))
 
+;; "The sentence is the executable element in a J script. Basically, it is
+;;  one line of code."
+;; "Each line is a J sentence, which may include, or be, a comment, or be
+;; blank or empty."
+;;
+;; (From: https://code.jsoftware.com/wiki/Vocabulary/Words)
 (defrule sentence
-    (? (or comment
-           (and (or whitespace
-                    subexpr
-                    string
-                    array
-                    primitive
-                    identifier)
-                sentence)))
+    (?
+     (or comment
+         commentless-sentence))
   (:function alexandria:flatten))
 
 (defrule commentless-sentence
-    (? (and (or whitespace
-                (and "(" commentless-sentence ")")
-                string
-                array
-                primitive
-                identifier)
-            commentless-sentence))
+    (and (or whitespace
+             (and "(" commentless-sentence ")")
+             string
+             ;; The PRIMITIVE rule must come before NUMBER and
+             ;; IDENTIFIER because they can be prefixes of primitives.
+             ;; Likewise, assignment operators should come before PRIMITIVE
+             ;; because of "=".
+             assignment
+             primitive
+             array
+             identifier)
+         (? sentence))
   (:function alexandria:flatten))
 
 (defrule comment (and "NB." (* (not nl)))
   (:text t))
 
-(defrule whitespace (or #\Space #\Tab)
+(defrule whitespace (+ (or #\Space #\Tab))
   (:text t))
-(defrule nl (or #\Return #\Linefeed)
+(defrule nl (or #\Return #\Linefeed #\Newline)
   (:text t))
 
-#|
-"Names (used for pronouns and other surrogates, and assigned referents by the copula, as in prices=: 4.5 12) begin with a letter and may continue with letters, underlines, and digits."
-|#
-(defrule identifier (and letter (+ (or #\_ letter digit)))
+;; "Names (used for pronouns and other surrogates, and assigned referents by
+;; the copula, as in prices=: 4.5 12) begin with a letter and may continue
+;; with letters, underlines, and digits."
+;;
+;; (From: https://wiki.jsoftware.com/wiki/Books/MathForTheLayman/Language_and_Grammar)
+(defrule identifier (and letter (* (or #\_ letter digit)))
   (:text t))
 (defrule letter (character-ranges (#\a #\z) (#\A #\Z)))
 
-#|
-"Numbers are denoted by digits, the underbar (for negative signs and for infinity and minus infinity -- when used alone or in pairs), the period (used for decimal points and necessarily preceded by one or more digits), the letter e (as in 2.4e3 to signify 2400 in exponential form), and the letter j to separate the real and imaginary parts of a complex number, as in 3e4j_0.56. Also see the discussion of Constants."
-|#
+;; "Numbers are denoted by digits, the underbar (for negative signs and for
+;; infinity and minus infinity -- when used alone or in pairs), the period
+;; (used for decimal points and necessarily preceded by one or more digits),
+;; the letter e (as in 2.4e3 to signify 2400 in exponential form), and the
+;; letter j to separate the real and imaginary parts of a complex number, as
+;; in 3e4j_0.56. Also see the discussion of Constants."
 (defrule number
     (or (and real-number (? (and "j" real-number)))
         "__"
@@ -73,7 +152,8 @@
   (:text t))
 (defrule digit (or "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"))
 
-;; "A list of ASCII characters is denoted by the list enclosed in single quotes, a pair of adjacent single quotes signifying the quote itself"
+;; "A list of ASCII characters is denoted by the list enclosed in single
+;; quotes, a pair of adjacent single quotes signifying the quote itself"
 (defrule string (and "'"
                      (+ (or "''" (not "'")))
                      "'")
@@ -85,15 +165,13 @@
          (* (and (+ " ") number)))
   (:text t))
 
-#|
-"A primitive or primary may be denoted by a single graphic (such as + for plus) or by a graphic modified by one or more following inflections (a period or colon), as in +. and +: for or and nor. A primary may also be an inflected name, as in e. and o. for membership and pi times. A primary cannot be assigned a referent."
-
-Also, see: https://code.jsoftware.com/wiki/NuVoc
-
-The PRIMITIVE rule must come before NUMBER and IDENTIFIER because some of
-their rules can be prefixes of these rules. Likewise, assignment operators
-should come before PRIMITIVE because of "=".
-|#
+;; "A primitive or primary may be denoted by a single graphic (such as +
+;; for plus) or by a graphic modified by one or more following inflections (a
+;; period or colon), as in +. and +: for or and nor. A primary may also
+;; be an inflected name, as in e. and o. for membership and pi times. A
+;; primary cannot be assigned a referent."
+;;
+;; Also, see: https://code.jsoftware.com/wiki/NuVoc
 (defrule primitive
     (or "="
         ;; Need to be careful that primitives that are prefixes of other
@@ -141,28 +219,18 @@ should come before PRIMITIVE because of "=".
         "u." "v." "a:" "a.")
   (:text t))
 
-;; TODO:
-;; Start putting it all together. Starting out with a single line ("sentence").
-;;   - brackets
-;;   - assignment
-;;   - control words (see "T-block" in notes)
-;;     if. else. elseif. assert. 
-;;     break. continue. for.
-;;     for_ijk. goto_lbl.
-;;     label_lbl.
-;;     return. select.
-;;     case.
-;;     fcase.
-;;     throw. catcht. try.
-;;     catch.
-;;     catchd.
-;;     catcht. while.
-;;     whilst.
-;;    - Various ways of defining functions
-;;     {{ }}
-;;     3 : 0
-;;     )
-;;    - Other weird 'm : n' constructs
-
+(defrule assignment
+    (and identifier (* whitespace) assignment-op commentless-sentence)
+  (:function make-assignment-node))
 (defrule assignment-op (or "=." "=:")
   (:text t))
+
+;; Different sentences are allowed inside definitions.
+;; TODO.
+(defrule def-sentences)
+
+(defrule if.
+    (and "if." def-sentences "do." def-sentences
+         (* (and "elseif." def-sentences "do." def-sentences))
+         (? (and "else." def-sentences))
+         "end."))
--- notes.md
+++ notes.md
@@ -1,24 +1,49 @@
 ## On parsing J
-Essential references:
+Some precise descriptions of the grammar. Quoted all the relevant parts
+in the grammar definition.
 
-* https://wiki.jsoftware.com/wiki/Books/MathForTheLayman/Language_and_Grammar
-* https://code.jsoftware.com/wiki/Vocabulary/Words#Sentence
-  "Each line is a J sentence, which may include, or be, a comment, or be blank or empty."
-  "Control words are allowed only inside the body of an explicit definition."
-  (Actually, this page is full of useful stuff)
-* https://code.jsoftware.com/wiki/Vocabulary/TBlock
-* https://wiki.jsoftware.com/wiki/User:Ian_Clark/BackusNaurFormOfJterms
-* https://code.jsoftware.com/wiki/Vocabulary/ifdot
-* https://code.jsoftware.com/wiki/Vocabulary/Glossary
-  "Explicit entities are created by the : conjunction or by its synonym define, with noun operands (m : n), or by direct definition by enclosing the text of the entity inside {{ }}."
-* https://code.jsoftware.com/wiki/Vocabulary/com
-  "m : n" syntax for defining functions.
+  https://wiki.jsoftware.com/wiki/Books/MathForTheLayman/Language_and_Grammar
 
+Defines various parts of the syntax and their semantics. Some relevant quotes included below, but best to read the whole thing.
+
+  https://code.jsoftware.com/wiki/Vocabulary/Words
+
+> The sentence is the executable element in a J script. Basically, it is one line of code.
+
+> Sentences cannot span more than one line. There is no "continuation character" as in Basic, and no "statement delimiter" as in Java or C. 
+
+> The executable line is treated as a single sentence, unless it contains control words, when the control words and sections of the line between control words are each treated as separate sentences. 
+
+> Each line is a J sentence, which may include, or be, a comment, or be blank or empty.
+
+> Control words are allowed only inside the body of an explicit definition.
+
+The IF statement.
+
+  https://code.jsoftware.com/wiki/Vocabulary/ifdot
+
+T-blocks, the "test" part of an IF. Can contain multiple sentences.
+
+  https://code.jsoftware.com/wiki/Vocabulary/TBlock
+
+Someone's attempt to define the J grammar. They didn't get very far.
+
+  https://wiki.jsoftware.com/wiki/User:Ian_Clark/BackusNaurFormOfJterms
+
+Glossary. "Explicit entities are created by the : conjunction or by its synonym define, with noun operands (m : n), or by direct definition by enclosing the text of the entity inside {{ }}."
+
+  https://code.jsoftware.com/wiki/Vocabulary/Glossary
+
+And this describes the "m : n" syntax for defining functions.
+
+  https://code.jsoftware.com/wiki/Vocabulary/com
+  
 Other:
 * https://www.jsoftware.com/help/dictionary/dict2.htm
 * https://code.jsoftware.com/wiki/Vocabulary/PartsOfSpeech
 
 
+## Misc scratchpad
 Inline noun:
    0 : 'hi'
 
@@ -33,11 +58,10 @@ Also:
         ...
     )
 
-Bookmarks I made:
-J closures
-	https://www.reddit.com/r/ProgrammingLanguages/comments/1gplj9l/comment/lws51ph/
-	https://code.jsoftware.com/wiki/User:Ric_Sherlock/XHTML/Script
-	https://code.jsoftware.com/wiki/Help/JforC/38_Parsing_and_Execution_I
-	https://code.jsoftware.com/wiki/Guides/Lexical_Closure
-	https://wiki.jsoftware.com/wiki/Guides/Language_FAQ/J_BNF
-	https://wiki.jsoftware.com/wiki/User:Raul_Miller/ParsingJ
+## Bookmarks
+https://www.reddit.com/r/ProgrammingLanguages/comments/1gplj9l/comment/lws51ph/
+https://code.jsoftware.com/wiki/User:Ric_Sherlock/XHTML/Script
+https://code.jsoftware.com/wiki/Help/JforC/38_Parsing_and_Execution_I
+https://code.jsoftware.com/wiki/Guides/Lexical_Closure
+https://wiki.jsoftware.com/wiki/Guides/Language_FAQ/J_BNF
+https://wiki.jsoftware.com/wiki/User:Raul_Miller/ParsingJ
